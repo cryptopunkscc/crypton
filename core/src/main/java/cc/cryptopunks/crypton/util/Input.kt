c4cf4c3cbe939548c21e3bef423dc99dff0825e4
yang <cryptopunkyang@pm.me>
2019-09-20 20:02:00 +0200

    Remove kache:android dependencies
    * upgrade dagger version

--- core/src/main/java/cc/cryptopunks/crypton/util/Input.kt
+++ core/src/main/java/cc/cryptopunks/crypton/util/Input.kt
@@ -1,32 +1,12 @@
 package cc.cryptopunks.crypton.util
 
-import android.content.SharedPreferences
-import cc.cryptopunks.kache.android.SharedPreferencesPersistent
 import cc.cryptopunks.kache.core.Kache
 import cc.cryptopunks.kache.core.invoke
 
 data class Input(
     val text: String = "",
     val error: String = ""
-) : SharedPreferencesPersistent {
-
-    override fun SharedPreferences.load(): SharedPreferencesPersistent? = Input(
-        text = getString(TEXT_KEY, "")!!,
-        error = getString(ERROR_KEY, "")!!
-    )
-
-    override fun SharedPreferences.Editor.save() {
-        putString(TEXT_KEY, text)
-        putString(ERROR_KEY, error)
-    }
-
-    companion object {
-        private const val TEXT_KEY = "text"
-        private const val ERROR_KEY = "error"
-
-        val EMPTY = Input()
-    }
-}
+)
 
 var Kache<Input>.text: String
     get() = value.text
