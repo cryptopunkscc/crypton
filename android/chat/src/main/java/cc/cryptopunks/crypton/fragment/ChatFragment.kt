39c73b10f72191a475a87fc8240f2b810074b206
yang <cryptopunkyang@pm.me>
2019-10-20 00:55:22 +0200

    Reduce ChatFragment dependencies

--- android/chat/src/main/java/cc/cryptopunks/crypton/fragment/ChatFragment.kt
+++ android/chat/src/main/java/cc/cryptopunks/crypton/fragment/ChatFragment.kt
@@ -3,12 +3,16 @@ package cc.cryptopunks.crypton.fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import cc.cryptopunks.crypton.api.Api
 import cc.cryptopunks.crypton.presentation.PresentationComponent
 import cc.cryptopunks.crypton.entity.Address
 import cc.cryptopunks.crypton.entity.Chat
+import cc.cryptopunks.crypton.entity.Message
+import cc.cryptopunks.crypton.navigation.Navigation
 import cc.cryptopunks.crypton.presenter.ChatPresenter
 import cc.cryptopunks.crypton.navigation.Route
 import cc.cryptopunks.crypton.presenter.Presenter
+import cc.cryptopunks.crypton.util.ExecutorsComponent
 import cc.cryptopunks.crypton.util.toMap
 import cc.cryptopunks.crypton.view.ChatView
 import dagger.Provides
@@ -21,7 +25,16 @@ class ChatFragment : PresenterFragment<
         ChatFragment.Component>() {
 
     @dagger.Component(
-        dependencies = [PresentationComponent::class],
+        dependencies = [
+            Address::class,
+            Api.Scope::class,
+            Message.Api::class,
+            Message.Repo::class,
+            Chat.Api::class,
+            Chat.Repo::class,
+            Navigation.Component::class,
+            ExecutorsComponent::class
+        ],
         modules = [Module::class]
     )
     interface Component : Presenter.Component<ChatPresenter>
@@ -31,15 +44,20 @@ class ChatFragment : PresenterFragment<
 
     override suspend fun onCreateComponent(
         component: PresentationComponent
-    ): Component = let {
+    ): Component = component.run {
         val route = Route.Chat(arguments.toMap())
         val chat = runBlocking(Dispatchers.IO) {
             component.chatRepo.get(Address.from(route.chatAddress))
         }
-        DaggerChatFragment_Component
-            .builder()
-            .presentationComponent(presentationComponent)
+        DaggerChatFragment_Component.builder()
             .module(Module(chat))
+            .executorsComponent(component)
+            .component(navigationComponent)
+            .address(address)
+            .repo(messageRepo)
+            .repo(chatRepo)
+            .api(component as Message.Api)
+            .api(component as Chat.Api)
             .build()
     }
 
