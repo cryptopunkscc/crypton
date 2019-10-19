63869bcc21641d8a4843532e1f5c688de407515d
yang <cryptopunkyang@pm.me>
2019-10-20 01:20:08 +0200

    Reduce RosterFragment dependencies

--- android/indicator/src/main/java/cc/cryptopunks/crypton/service/IndicatorService.kt
+++ android/indicator/src/main/java/cc/cryptopunks/crypton/service/IndicatorService.kt
@@ -3,8 +3,6 @@ package cc.cryptopunks.crypton.service
 import android.app.IntentService
 import android.app.Service
 import android.content.Intent
-import cc.cryptopunks.crypton.component.DaggerIndicatorComponent
-import cc.cryptopunks.crypton.component.IndicatorComponent
 import cc.cryptopunks.crypton.component.ServiceComponent
 import cc.cryptopunks.crypton.entity.Indicator
 import cc.cryptopunks.crypton.module.ServiceModule
@@ -15,12 +13,12 @@ class IndicatorService : IntentService(Indicator.serviceName) {
 
     @dagger.Component(dependencies = [ServiceComponent::class])
     internal interface Component {
-        val setupNotificationChannel: CreateNotificationChannel
+        val createNotificationChannel: CreateNotificationChannel
         val showIndicatorNotification: ShowIndicatorNotification
     }
 
-    private val component: IndicatorComponent by lazy {
-        DaggerIndicatorComponent.builder()
+    private val component: Component by lazy {
+        DaggerIndicatorService_Component.builder()
             .serviceComponent(ServiceModule(this))
             .build()
     }
--- android/roster/src/main/java/cc/cryptopunks/crypton/fragment/RosterFragment.kt
+++ android/roster/src/main/java/cc/cryptopunks/crypton/fragment/RosterFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import cc.cryptopunks.crypton.entity.Chat
+import cc.cryptopunks.crypton.entity.Message
+import cc.cryptopunks.crypton.navigation.Navigation
 import cc.cryptopunks.crypton.presentation.PresentationComponent
 import cc.cryptopunks.crypton.presenter.RosterPresenter
 import cc.cryptopunks.crypton.presenter.Presenter
+import cc.cryptopunks.crypton.util.ExecutorsComponent
 import cc.cryptopunks.crypton.view.RosterView
 import javax.inject.Singleton
 
@@ -17,7 +21,12 @@ class RosterFragment : PresenterFragment<
 
     @Singleton
     @dagger.Component(
-        dependencies = [PresentationComponent::class]
+        dependencies = [
+            ExecutorsComponent::class,
+            Navigation.Component::class,
+            Chat.Repo::class,
+            Message.Repo::class
+        ]
     )
     interface Component : Presenter.Component<RosterPresenter>
 
@@ -25,7 +34,10 @@ class RosterFragment : PresenterFragment<
         component: PresentationComponent
     ): Component = DaggerRosterFragment_Component
         .builder()
-        .presentationComponent(component)
+        .executorsComponent(component)
+        .component(navigationComponent)
+        .repo(component.chatRepo)
+        .repo(component.messageRepo)
         .build()
 
     override fun onCreateView(
