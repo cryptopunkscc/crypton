4447556822d06f0a81615a40083fb7b761facd00
yang <cryptopunkyang@pm.me>
2019-11-19 16:02:39 +0100

    Filter messages with not text

--- net/smack/src/main/java/cc/cryptopunks/crypton/smack/util/ext/Stanza.kt
+++ net/smack/src/main/java/cc/cryptopunks/crypton/smack/util/ext/Stanza.kt
@@ -7,7 +7,7 @@ import org.jivesoftware.smackx.omemo.element.OmemoElement
 import org.jivesoftware.smackx.omemo.element.OmemoElement_VAxolotl
 
 fun <T: Stanza> T.replaceBody(decrypted: OmemoMessage.Received) =
-    if (this !is Message) null
+    if (this !is Message || decrypted.body.isBlank()) null
     else apply {
         removeOmemoBody()
         body = decrypted.body
