032c9c71e3b1174ee08f446139574d52be9b688e
yang <cryptopunkyang@pm.me>
2019-11-21 16:58:25 +0100

    Add chat type to chat messages

--- net/smack/src/main/java/cc/cryptopunks/crypton/smack/net/chat/SendMessage.kt
+++ net/smack/src/main/java/cc/cryptopunks/crypton/smack/net/chat/SendMessage.kt
@@ -2,6 +2,7 @@ package cc.cryptopunks.crypton.smack.net.chat
 
 import cc.cryptopunks.crypton.entity.Address
 import cc.cryptopunks.crypton.entity.Message.Net.Send
+import org.jivesoftware.smack.packet.Message
 import org.jivesoftware.smack.roster.Roster
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
 import org.jivesoftware.smackx.omemo.OmemoManager
@@ -14,13 +15,16 @@ internal class SendMessage(
     encryptedMessageCache: EncryptedMessageCache
 ) : Send, (Address, String) -> Unit by { to, text ->
 
-    val jid = JidCreate.bareFrom(to)
+    val jid = JidCreate.entityBareFrom(to)
 
     if (!roster.iAmSubscribedTo(jid)) {
         roster.createEntry(jid, to.local, emptyArray())
     }
 
-    val message = omemoManager.encrypt(jid, text).asMessage(jid)
+    val message = omemoManager
+        .encrypt(jid, text)
+        .asMessage(jid)
+        .apply { type = Message.Type.chat }
     encryptedMessageCache[message.stanzaId] = text
 //    val message = Message(jid, text)
     connection.sendStanza(message)
