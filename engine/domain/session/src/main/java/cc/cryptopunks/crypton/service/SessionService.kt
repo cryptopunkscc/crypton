b8cc7b1d3a7b5720762c753a56e5230126e32341
yang <cryptopunkyang@pm.me>
2020-06-22 04:02:29 +0200

    Refactor sessions service

--- engine/domain/session/src/main/java/cc/cryptopunks/crypton/service/SessionService.kt
+++ engine/domain/session/src/main/java/cc/cryptopunks/crypton/service/SessionService.kt
@@ -4,27 +4,20 @@ import cc.cryptopunks.crypton.context.SessionScope
 import cc.cryptopunks.crypton.context.collect
 import cc.cryptopunks.crypton.handler.handleApiEvent
 import cc.cryptopunks.crypton.handler.handleFlushQueuedMessages
-import cc.cryptopunks.crypton.handler.handleOmemoInitialized
 import cc.cryptopunks.crypton.handler.handlePresenceChanged
-import cc.cryptopunks.crypton.interactor.saveMessage
-import cc.cryptopunks.crypton.interactor.updateChatNotification
+import cc.cryptopunks.crypton.handler.handleSaveMessages
+import cc.cryptopunks.crypton.handler.handleUpdateChatNotification
 import cc.cryptopunks.crypton.selector.flushQueueMessagesFlow
-import cc.cryptopunks.crypton.selector.omemoInitializations
 import cc.cryptopunks.crypton.selector.presenceChangedFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.fold
+import cc.cryptopunks.crypton.selector.saveMessagesFlow
+import cc.cryptopunks.crypton.selector.updateChatNotificationFlow
 import kotlinx.coroutines.launch
 
 fun SessionScope.startSessionService() = launch {
     log.d("Invoke session services for $address")
     launch { netEvents().collect(handleApiEvent()) }
-    launch { omemoInitializations().collect(handleOmemoInitialized()) }
-    launch {
-        messageRepo.unreadListFlow().fold(updateChatNotification()) { update, messages ->
-            update.also { it(messages) }
-        }
-    }
-    launch { incomingMessages().collect { event -> saveMessage(event.message) } }
-    launch { presenceChangedFlow().collect(handlePresenceChanged()) }
+    launch { updateChatNotificationFlow().collect(handleUpdateChatNotification() ) }
+    launch { saveMessagesFlow().collect(handleSaveMessages(), join = true) }
+    launch { presenceChangedFlow().collect(handlePresenceChanged(), join = true) }
     launch { flushQueueMessagesFlow().collect(handleFlushQueuedMessages(), join = true) }
 }
