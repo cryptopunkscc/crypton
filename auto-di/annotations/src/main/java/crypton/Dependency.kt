91c8bb1883aedd63a0d05a524e256594648293d4
yang <cryptopunkyang@pm.me>
2020-06-26 21:15:23 +0200

    Remove auto-di legacy

--- auto-di/annotations/src/main/java/crypton/Dependency.kt
+++
@@ -1,5 +0,0 @@
-package crypton
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.CLASS)
-annotation class Dependency
--- auto-di/annotations/src/main/java/crypton/External.kt
+++
@@ -1,5 +0,0 @@
-package crypton
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.PROPERTY_GETTER)
-annotation class External
