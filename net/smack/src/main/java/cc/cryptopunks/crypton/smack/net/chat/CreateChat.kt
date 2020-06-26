8f6d684f01e722dfce1bb9c3ab42e600bf912a31
yang <cryptopunkyang@pm.me>
2020-06-26 21:40:06 +0200

    Small update

--- net/smack/src/main/java/cc/cryptopunks/crypton/smack/net/chat/CreateChat.kt
+++ net/smack/src/main/java/cc/cryptopunks/crypton/smack/net/chat/CreateChat.kt
@@ -8,13 +8,12 @@ import org.jxmpp.jid.parts.Resourcepart
 internal fun SmackCore.createChat(
     chat: Chat
 ) = chat.also {
-    mucManager
-        .getMultiUserChat(JidCreate.entityBareFrom(chat.address.toString()))
-        .apply {
-            chat.users.dropLast(1).forEach { user ->
-                invite(JidCreate.entityBareFrom(user.address.toString()), "")
-            }
+    require(chat.title.isNotBlank())
+
+
+    mucManager.getMultiUserChat(JidCreate.entityBareFrom(chat.address.toString())).apply {
+        chat.users.filterNot { it.address == chat.account }.forEach { user ->
+            invite(JidCreate.entityBareFrom(user.address.id), "")
         }
-        .create(Resourcepart.from(chat.accountUser.address.local))
-        .makeInstant()
+    }.create(Resourcepart.from(chat.accountUser.address.local)).makeInstant()
 }
