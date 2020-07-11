f06500e3f5bf35f90f7cd43e0153a3f9084179fc
yang <cryptopunkyang@pm.me>
2020-07-11 14:48:28 +0200

    Add translateMessage function for message input

--- jvm/translator/src/main/java/cc/cryptopunks/crypton/CliTranslator.kt
+++ jvm/translator/src/main/java/cc/cryptopunks/crypton/CliTranslator.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.scan
 
-fun Flow<Any>.translate(context: Context = Context()) =
+fun Flow<Any>.translateCli(context: Context = Context()): Flow<Any> =
     scan(context.prepare()) { ctx, input ->
         when(input) {
             is String -> ctx.process(input)
@@ -12,4 +12,10 @@ fun Flow<Any>.translate(context: Context = Context()) =
         }
     }.mapNotNull {
         it.result
+    }
+
+fun Context.translateMessage(message: String): Any =
+    when (message.startsWith("/")) {
+        false -> message
+        true -> process(message)
     }
