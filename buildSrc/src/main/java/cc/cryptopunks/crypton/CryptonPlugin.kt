aa3002c79f4c4e9d52b8557f5e5460b01eb79f00
yang <cryptopunkyang@pm.me>
2020-07-15 18:28:26 +0200

    Add pumlToSvg gradle task & fix dependencies

--- buildSrc/src/main/java/cc/cryptopunks/crypton/CryptonPlugin.kt
+++ buildSrc/src/main/java/cc/cryptopunks/crypton/CryptonPlugin.kt
@@ -2,19 +2,27 @@ package cc.cryptopunks.crypton
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Exec
 import org.gradle.kotlin.dsl.invoke
+import java.io.File
 
 class CryptonPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.run {
             val context = extensions.create("crypton", CryptonExtension::class.java)
             tasks {
-                register("modulesUml", ModuleUmlTask::class.java) {
+                register("redundantDependencies", RedundantDependenciesTask::class.java) {
+                    group = "help"
+                }
+                register("modulesPuml", ModuleUmlTask::class.java) {
                     extension = context
                     group = "documentation"
                 }
-                register("redundantDependencies", RedundantDependenciesTask::class.java) {
-                    group = "help"
+                register("pumlToSvg", Exec::class.java) {
+                    group = "documentation"
+                    logger.quiet(File("").absolutePath)
+                    commandLine("java", "-jar", "./libs/plantuml.jar", "./docs/modules/puml/*.puml", "-svg", "-o", "../svg/")
+                    dependsOn("modulesPuml")
                 }
             }
         }
